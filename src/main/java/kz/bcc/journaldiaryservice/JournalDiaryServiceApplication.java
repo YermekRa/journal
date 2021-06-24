97f40bd594de34a3395488ba440c68aa8db8a63f
TEMIRLAN <temirlanturikanov@gmail.com>
2021-06-24 10:18:12 +0600

    first commit

---
+++ src/main/java/kz/bcc/journaldiaryservice/JournalDiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.bcc.journaldiaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalDiaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalDiaryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bcc/journaldiaryservice/JournalDiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bcc.journaldiaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalDiaryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
