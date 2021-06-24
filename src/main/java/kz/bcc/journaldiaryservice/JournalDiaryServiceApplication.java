ac62ebfe3ce808a2c9bd17d2c47d140cb168a876
TEMIRLAN <temirlanturikanov@gmail.com>
2021-06-24 11:21:25 +0600

    add EurekaClient

--- src/main/java/kz/bcc/journaldiaryservice/JournalDiaryServiceApplication.java
+++ src/main/java/kz/bcc/journaldiaryservice/JournalDiaryServiceApplication.java
@@ -2,12 +2,18 @@ package kz.bcc.journaldiaryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+//@EnableCircuitBreaker
 public class JournalDiaryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalDiaryServiceApplication.class, args);
+
     }
+
 
 }
